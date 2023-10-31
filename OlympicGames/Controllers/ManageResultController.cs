e4aab20dac186e28c1312a575405128a815268d6
DARP\kevin.montoya <kevin.montoya@vasscompany.com>
2023-10-31 18:47:35 -0500

    Ordenamiento de Resultados por Criterios establecidos en las Especificaciones

---
+++ OlympicGames/Controllers/ManageResultController.cs
@@ -0,0 +1,26 @@
+﻿using OlympicGames.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OlympicGames.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ManageResultController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult GetResults([FromBody] List<ResultHalterofilia> results)
+        {
+            results = results.GroupBy(r => new { r.Pais, r.Nombre })
+            .Select(group => new ResultHalterofilia
+            {
+                Pais = group.Key.Pais,
+                Nombre = group.Key.Nombre,
+                Arranque = group.Max(r => r.Arranque),
+                Envion = group.Max(r => r.Envion),
+            }).OrderByDescending(r => r.TotalPeso).ToList();
+
+            return Ok(results);
+        }
+    }
+}
---
+++ OlympicGames/Models/ResultHalterofilia.cs
@@ -0,0 +1,14 @@
+﻿namespace OlympicGames.Models
+{
+    public class ResultHalterofilia
+    {
+        public string Pais { get; set; }
+        public string Nombre { get; set; }
+        public int Arranque { get; set; }
+        public int Envion { get; set; }
+        public int TotalPeso
+        {
+            get { return Arranque + Envion; }
+        }
+    }
+}
